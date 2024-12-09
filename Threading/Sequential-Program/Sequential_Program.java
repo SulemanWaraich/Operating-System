b21eec85ce6ce0b6db796212778face2bd97ebc3
Muhammad Suleman <145265998+SulemanWaraich@users.noreply.github.com>
2024-12-10 00:31:38 +0500

    Sequential-Program

---
+++ Threading/Sequential-Program/Sequential_Program.java
@@ -0,0 +1,21 @@
+public class Sequential_Program {
+
+    static void performTask1() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Task 1: " + i);
+        }
+    }
+
+    static void performTask2() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Task 2: " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        performTask1();
+        performTask2();
+    }
+
+}
+
